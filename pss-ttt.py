1b40aec10988fc1ecc6af74263940461ec3048b2
arraken <52732965+arraken@users.noreply.github.com>
2024-06-11 23:15:48 -0500

    1.4.7

--- pss-ttt.py
+++ pss-ttt.py
@@ -11,7 +11,7 @@ import time, logging
 from pssapi import PssApiClient
 
 ACCESS_TOKEN = None
-CURRENT_VERSION = "v1.4.2"
+CURRENT_VERSION = "v1.4.7"
 CREATOR = "Kamguh11"
 SUPPORT_LINK = "Trek Discord - https://discord.gg/psstrek or https://discord.gg/pss"
 GITHUB_LINK = "https://github.com/arraken/pss-ttt"
